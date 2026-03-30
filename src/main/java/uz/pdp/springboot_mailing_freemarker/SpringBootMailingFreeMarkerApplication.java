65ae05a95a7d03bc6ec79c9556fa1556d6cadec9
Salohiddin Yunusov <salohiddinyunusov304@gmail.com>
2026-03-30 18:41:31 +0500

    mailing and freeMarker

---
+++ src/main/java/uz/pdp/springboot_mailing_freemarker/SpringBootMailingFreeMarkerApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springboot_mailing_freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMailingFreeMarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMailingFreeMarkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/springboot_mailing_freemarker/SpringBootMailingFreeMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.springboot_mailing_freemarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMailingFreeMarkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
