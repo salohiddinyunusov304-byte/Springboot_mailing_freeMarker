87b08cb3cf6b99b4a6e5e3efca2a86bc016832b1
Salohiddin Yunusov <salohiddinyunusov304@gmail.com>
2026-03-31 10:06:20 +0500

    Spring boot mailing freeMaker is starting...

--- src/main/java/uz/pdp/springboot_mailing_freemarker/SpringBootMailingFreeMarkerApplication.java
+++ src/main/java/uz/pdp/springboot_mailing_freemarker/SpringBootMailingFreeMarkerApplication.java
@@ -1,13 +1,107 @@
 package uz.pdp.springboot_mailing_freemarker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Working with Mailing and freeMarker| G58 Open Specification | Swagger",
+                version = "1.0",
+                description = "API for using Mailing and freeMarker",
+                contact = @Contact(
+                        name = "Salohiddin Yunusov",
+                        email = "salohiddinyunusov377@gmail.com"
+                ),
+                license = @License(
+                        name = "MIT License",
+                        url = "https://opensource.org/licenses/MIT"
+                )
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:8080",
+                        description = "Local Server"
+                )
+        }
+)
+
+@ConfigurationPropertiesScan
+@EnableAsync
 public class SpringBootMailingFreeMarkerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMailingFreeMarkerApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Spring Swagger Example")
+                        .description("Spring Boot + Swagger + MapStruct Example")
+                        .version("1.0")
+                        .contact(new io.swagger.v3.oas.models.info.Contact()
+                                .name("Salohiddin Yunusov")
+                                .email("salohiddinyunusov377@gmail.com")
+                                .url("https://github.com"))
+                        .license(new io.swagger.v3.oas.models.info.License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org"))
+                        .termsOfService("http://swagger.io/terms/"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentation")
+                        .url("https://springdoc.org"))
+                .servers(List.of(
+                        new io.swagger.v3.oas.models.servers.Server()
+                                .url("http://localhost:8080")
+                                .description("Production"),
+                        new io.swagger.v3.oas.models.servers.Server()
+                                .url("http://localhost:9090")
+                                .description("Test Server")
+                ));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+    @Bean
+    @Profile("prod")
+    public TaskExecutor taskExecutorProd() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+
+        taskExecutor.setCorePoolSize(10);
+        taskExecutor.setMaxPoolSize(100);
+        taskExecutor.setKeepAliveSeconds(30);
+        taskExecutor.setQueueCapacity(100);
+        taskExecutor.setThreadNamePrefix("Prod-");
+        return taskExecutor;
     }
 
 }
